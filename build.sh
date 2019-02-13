72f3f7aa9cc01fec29ef98ebf01d919f5e3bd183
Knut Ahlers <knut@ahlers.me>
2019-02-13 16:39:10 +0100

    Fix: Download jq before using it
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- build.sh
+++ build.sh
@@ -12,15 +12,15 @@ apt-get install -y --no-install-recommends \
 	git \
 	make
 
+# Download recent release of jq
+curl -sSfLo /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod 0755 /usr/local/bin/jq
+
 # Download latest release of inner-runner
 DOWNLOAD=$(curl -sSfL https://api.github.com/repos/repo-runner/repo-runner/releases/latest |
 	jq -r '.assets | .[] | select(.name == "inner-runner_linux_amd64.tar.gz") | .browser_download_url')
 curl -sSfL "${DOWNLOAD}" | tar -xzf - -C /usr/local/bin
 mv /usr/local/bin/inner-runner* /usr/local/bin/inner-runner
-
-# Download recent release of jq
-curl -sSfLo /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-chmod 0755 /usr/local/bin/jq
 
 # Cleanup
 apt-get autoremove --purge -y
