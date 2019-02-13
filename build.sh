f3f3c4ce39b592fc63819bf82640978752b4f363
Knut Ahlers <knut@ahlers.me>
2019-02-13 16:37:04 +0100

    Use jq 1.6
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- build.sh
+++ build.sh
@@ -6,18 +6,21 @@ export DEBIAN_FRONTEND=noninteractive
 # Install required utils
 apt-get update
 apt-get install -y --no-install-recommends \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  git \
-  jq \
-  make
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	git \
+	make
 
 # Download latest release of inner-runner
 DOWNLOAD=$(curl -sSfL https://api.github.com/repos/repo-runner/repo-runner/releases/latest |
 	jq -r '.assets | .[] | select(.name == "inner-runner_linux_amd64.tar.gz") | .browser_download_url')
 curl -sSfL "${DOWNLOAD}" | tar -xzf - -C /usr/local/bin
 mv /usr/local/bin/inner-runner* /usr/local/bin/inner-runner
+
+# Download recent release of jq
+curl -sSfLo /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod 0755 /usr/local/bin/jq
 
 # Cleanup
 apt-get autoremove --purge -y
